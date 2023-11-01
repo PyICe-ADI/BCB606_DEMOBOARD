9568dd746c4648edaf75cf2a31aa25a78fb06f60
Steve Martin <Xenomorphxx131@gmail.com>
2023-11-01 07:07:18 -0400

    Typo in query

--- Python/EZ_BCB606.py
+++ Python/EZ_BCB606.py
@@ -4,7 +4,7 @@ from PyICe_driver.BCB606 import BCB606
 from PyICe import lab_core
 from PyICe.lab_utils.banners import print_banner
 
-print_banner("","Please enter the COM port number for your BCB606.", "It can be found obtained from the Windows Device Manager.","")
+print_banner("","Please enter the COM port number for your BCB606.", "It can be obtained from the Windows Device Manager.","")
 comport = input("Enter COM port number, COM")
 
 channel_master = lab_core.channel_master()
