574dfdadf34aeb16a743ad4ab4a673be6aeb03ea
Steve Martin <Xenomorphxx131@gmail.com>
2023-11-01 07:02:12 -0400

    Now inquires about the COM port address before starting.

--- Python/EZ_BCB606.py
+++ Python/EZ_BCB606.py
@@ -2,9 +2,13 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from PyICe_driver.BCB606 import BCB606
 from PyICe import lab_core
+from PyICe.lab_utils.banners import print_banner
+
+print_banner("","Please enter the COM port number for your BCB606.", "It can be found obtained from the Windows Device Manager.","")
+comport = input("Enter COM port number, COM")
 
 channel_master = lab_core.channel_master()
-BCB606_board = BCB606(channel_master, comport="COM18", verbose=False)
+BCB606_board = BCB606(channel_master, comport=f"COM{comport}", verbose=False)
 channel_master.add(BCB606_board)
 
 channel_master.write("BCB606_enable", "OFF") # Board is stateful
