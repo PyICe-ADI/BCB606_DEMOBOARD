ef2c30e42b58ee81d80267a09fa1760c481858e1
Steve Martin <Xenomorphxx131@gmail.com>
2024-05-29 16:04:27 -0400

    Minor cleanup

--- Python/EZ_BCB606.py
+++ Python/EZ_BCB606.py
@@ -6,11 +6,9 @@ from PyICe.lab_utils.banners import print_banner
 
 print_banner("", "Please enter the COM port number for your BCB606.", "", "It can be obtained from the Windows Device Manager.", "")
 comport = input("Enter COM port number, COM")
-
 channel_master = lab_core.channel_master()
 BCB606_board = BCB606(channel_master, comport=f"COM{comport}", verbose=False)
 channel_master.add(BCB606_board)
-
 channel_master.write("BCB606_enable", "OFF") # Board is stateful
 channel_master.write("BCB606_wd_svc_time", 16000)
 channel_master.write("BCB606_wd_svc_enable", True)
